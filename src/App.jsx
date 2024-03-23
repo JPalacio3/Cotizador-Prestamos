4f4bf4f99bd10252f7598da0335588bbbd147a23
master <joelpalacio630@gmail.com>
2024-03-22 19:56:00 -0600

    feat : Realizando el cálculo del total a pagar en base a los plazos seleccionados - Incorporación de useEffect para actualizar automaticamente la cantidad en pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from "./components/Header";
 import Button from "./components/Button";
-import { formatearDinero } from './helpers'
+import { formatearDinero, calcularTotalPagar } from './helpers'
 
 function App() {
 
   const [ cantidad, setCantidad ] = useState( 10000 );
   const [ meses, setMeses ] = useState( 12 );
+  const [ total, setTotal ] = useState( 0 );
+
+  useEffect( () => {
+    const resultadoTotalPagar = calcularTotalPagar( cantidad, meses );
+    setTotal( resultadoTotalPagar );
+  }, [ cantidad, meses ] );
 
   const MIN = 1000;
   const MAX = 20000;
@@ -70,6 +76,7 @@ function App() {
         Elige un <span className='text-indigo-600'>Plazo</span> a pagar
       </h2>
 
+      {/* Select para el tiempo de pago */}
       <select name="" id="" className='mt-5 w-full bg-white border border-gray-300 rounded-lg text-center font-bold text-xl text-gray-500' value={meses} onChange={e => setMeses( +e.target.value )}>
         <option value="" disabled='disabled'> Seleccione</option>
         <option value="6"> 6 Meses</option>
@@ -77,9 +84,16 @@ function App() {
         <option value="24"> 24 Meses</option>
       </select>
 
+      {/* Resúmen del préstamo  */}
+      <div className="my-5 space-y-3 bg-gray-50 py-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span className='text-indigo-600'>De Pagos</span>
+        </h2>
+        <p className="text-xl text-gray-500 text-center font-bold">{meses} Meses</p>
+        <p className="text-xl text-gray-500 text-center font-bold">Total a Pagar {formatearDinero( total )}</p>
+        <p className="text-xl text-gray-500 text-center font-bold">Mensuales</p>
 
-
-
+      </div>
     </div >
   )
 }
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,5 @@
 
-// Función encargade de formatar la cantidad en dinero
+// Función encargada de formatar la cantidad en dinero
 const formatearDinero = ( valor ) => {
     const formatter = new Intl.NumberFormat( 'en-US', {
         style: 'currency',
@@ -8,6 +8,36 @@ const formatearDinero = ( valor ) => {
     return formatter.format( valor );
 }
 
+// Función encargada de realizar el cálculo de los pagos mensuales
+const calcularTotalPagar = ( cantidad, plazo ) => {
+    let total;
+
+    // Mentras mayor sea la cantidad solicitada, menor es el interés
+    if ( cantidad < 5000 ) {
+        total = cantidad * 1.5;
+    } else if ( cantidad >= 5000 && cantidad < 10000 ) {
+        total = cantidad * 1.3;
+    } else if ( cantidad >= 10000 && cantidad < 15000 ) {
+        total = cantidad * 1.2;
+    } else {
+        total = cantidad * 1.1;
+    }
+
+    // Mientras mayor sea el plazo, el interés será más alto
+    if ( plazo === 6 ) {
+        total *= 1.1;
+    } else if ( plazo === 12 ) {
+        total *= 1.2;
+    } else {
+        total *= 1.3;
+    }
+    return total;
+}
+
+
+
+
 export {
-    formatearDinero
+    formatearDinero,
+    calcularTotalPagar
 }
