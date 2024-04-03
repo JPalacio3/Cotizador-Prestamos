b3203246504a99883461299ce360e47e6120fa62
master <joelpalacio630@gmail.com>
2024-04-03 14:59:13 -0600

    feat: Añadiendo el computed property para formatear la cantidad del range a dinero en dólares

--- cotizador-prestamos-vue/src/App.vue
+++ cotizador-prestamos-vue/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+
 import Header from './components/Header.vue';
 
 const cantidad = ref( 10000 );
@@ -7,21 +8,24 @@ const MIN = 0;
 const MAX = 20000;
 const STEP = 500;
 
+const formatearDinero = computed( () => {
+    const formatter = new Intl.NumberFormat( 'en-US', {
+        style: 'currency',
+        currency: 'USD'
+
+    } )
+    return formatter.format( cantidad.value );
+} );
+
 </script>
 
 <template>
-
     <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
         <Header />
-
         <div class="my-5">
             <input type="range" class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600 " :min="MIN" :max="MAX"
                 :step="STEP" v-model.number="cantidad" />
-            <p>$ {{ cantidad }}</p>
-
-
-
-
+            <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{ formatearDinero }}</p>
         </div>
     </div>
 
