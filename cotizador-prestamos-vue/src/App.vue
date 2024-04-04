6fd8cd85d664e301298b9e304d895abeaf9c95fb
JPalacio <joelpalacio630@gmail.com>
2024-04-03 20:53:01 -0600

    feat: Implementando la función encargada de calcular el total a pagar en base al interes de prestamo y tiempo a pagar

--- cotizador-prestamos-vue/src/App.vue
+++ cotizador-prestamos-vue/src/App.vue
@@ -1,22 +1,30 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 import Header from './components/Header.vue';
 import Button from './components/Button.vue';
+import { calcularTotalPagar } from './helpers'
 
 const cantidad = ref( 10000 );
+const meses = ref( 12 );
+const total = ref( calcularTotalPagar( cantidad.value, meses.value ) );
 const MIN = 500;
 const MAX = 20000;
 const STEP = 500;
 
-const formatearDinero = computed( () => {
+const formatearDinero = ( valor ) => {
     const formatter = new Intl.NumberFormat( 'en-US', {
         style: 'currency',
         currency: 'USD'
+    } );
+    return formatter.format( valor );
+};
 
-    } )
-    return formatter.format( cantidad.value );
+watch( [ cantidad, meses ], () => {
+    total.value = calcularTotalPagar( cantidad.value, meses.value );
 } );
+
+
 
 const handleChangeDecrementor = () => {
     if ( cantidad.value <= MIN ) {
@@ -42,16 +50,41 @@ const handleChangeIncrementor = () => {
         <Header />
 
         <div class="flex justify-between mt-10">
-            <Button :operador="'-'" :fn="handleChangeDecrementor" />
-            <Button :operador="'+'" :fn="handleChangeIncrementor" />
+            <Button :operador="'-'" @fn="handleChangeDecrementor" />
+            <Button :operador="'+'" @fn="handleChangeIncrementor" />
         </div>
 
         <div class="my-5">
             <input type="range" class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600 " :min="MIN" :max="MAX"
                 :step="STEP" v-model.number="cantidad" />
 
-            <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{ formatearDinero }}</p>
+            <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{ formatearDinero(cantidad) }}</p>
+
+            <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+                Elige un <span class="text-indigo-600">Plazo</span> a Pagar
+            </h2>
+
+            <select name="" id=""
+                class="w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500 mt-7"
+                :value="meses" v-model.number="meses">
+                <option value="#" selected disabled class="text-gray-400"> Seleccione</option>
+                <option value="6"> 6 Meses</option>
+                <option value="12"> 12 Meses</option>
+                <option value="24"> 24 Meses</option>
+            </select>
+        </div>
+
+        <div class="my-5 space-y-3 bg-gray-50 p-5">
+            <h2 class="text-2xl font-extrabold text-gray-500 text-center">
+                Resumen <span class="text-indigo-600">De Pagos</span>
+            </h2>
+            <p class="text-xl text-gray-500 text-center font-bold"><span class="text-indigo-600">{{ meses }}</span>
+                Meses</p>
+            <p class="text-xl text-gray-500 text-center font-bold">Total a pagar: <span
+                    class="text-indigo-600">{{ formatearDinero(total) }}</span></p>
+            <p class="text-xl text-gray-500 text-center font-bold">Mensuales</p>
+
+
         </div>
     </div>
-
 </template>
--- cotizador-prestamos-vue/src/components/Button.vue
+++ cotizador-prestamos-vue/src/components/Button.vue
@@ -1,13 +1,11 @@
 <script setup>
 const props = defineProps( {
-    operador: String,
-    fn: Function
+    operador: String
 } );
 </script>
 
 <template>
     <button type="button"
         class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
-        @click="fn">{{
-            operador }}</button>
+        @click="$emit('fn')">{{ operador }}</button>
 </template>
